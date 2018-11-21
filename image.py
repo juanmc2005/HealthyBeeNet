335f07ad574a724507dfd7efcc0b859cb0bbd75a
Juanma Coria <juanmc2005@hotmail.com>
2018-11-21 21:16:54 +0100

    Added functions to apply sift to image dataset and collect descriptors in a matrix

--- image.py
+++ image.py
@@ -1,10 +1,20 @@
 import cv2
 
-image = cv2.imread("bee_dataset/bee_imgs/001_044.png")
-color = cv2.cvtColor(image, cv2.IMREAD_COLOR)
-sift = cv2.xfeatures2d.SIFT_create()
-(kps, descs) = sift.detectAndCompute(color, None)
-cv2.drawKeypoints(color, kps, image, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imwrite('sift_keypoints.jpg', image)
-print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
-print(descs)
+
+def sift(image):
+    """
+    Applies the SIFT algorithm to an image
+    :param image: the path to a bee image
+    :return: a matrix of shape (k, 128), where k is the number
+        of key points detected by SIFT, with the descriptors for every key point
+    """
+    image = cv2.imread(image)
+    color = cv2.cvtColor(image, cv2.IMREAD_COLOR)
+    op = cv2.xfeatures2d.SIFT_create()
+    kp, descriptors = op.detectAndCompute(color, None)
+    return descriptors
+
+
+# Draw key points to inspect image TODO make a function
+# cv2.drawKeypoints(color, kps, image, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# cv2.imwrite('sift_keypoints.jpg', image)
--- playground.py
+++ playground.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+from os import listdir
+from os.path import join
+import scipy.io as sio
+import image
 
 
-def read_dataset(filename):
+def read_metadataset(filename):
     """
     Reads the bee image dataset csv file
     :param filename: The name of the csv file
@@ -22,4 +26,27 @@ def labels(data):
     return [value == 'healthy' for value in data['health']]
 
 
-print(np.mean(labels(read_dataset('bee_dataset/bee_data.csv'))))
+def compute_descriptors(dir, out_file=None):
+    """
+    Transform a bee image dataset into a matrix representing all key
+    point descriptors detected by SIFT.
+    :param dir: the source directory where all the images are located
+    :param out_file: an optional file path to store the descriptors in binary format
+    :return: a matrix of shape (k, 128), where k is the number of
+        key points extracted by SIFT
+    """
+    files = listdir(dir)
+    result = image.sift(join(dir, files[0]))
+    for file in files[1:]:
+        descriptors = image.sift(join(dir, file))
+        if descriptors is not None:
+            result = np.append(result, descriptors, axis=0)
+        else:
+            print("No descriptors found for " + file)
+    if out_file is not None:
+        np.save(out_file, result)
+    return result
+
+
+# print(np.mean(labels(read_metadataset('bee_dataset/bee_data.csv'))))
+print(np.load('bee_dataset/sift_descriptors.npy').shape)
