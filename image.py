cea8bc4872cc7039590271af626d13ae76a990bb
Juanma Coria <juanmc2005@hotmail.com>
2018-11-21 13:48:54 +0100

    Added code to draw SIFT keypoints over a target image

--- image.py
+++ image.py
@@ -1,8 +1,10 @@
 import cv2
 
-image = cv2.imread("bee_dataset/bee_imgs/001_043.png")
-gray = cv2.cvtColor(image, cv2.IMREAD_COLOR)
+image = cv2.imread("bee_dataset/bee_imgs/001_044.png")
+color = cv2.cvtColor(image, cv2.IMREAD_COLOR)
 sift = cv2.xfeatures2d.SIFT_create()
-(kps, descs) = sift.detectAndCompute(gray, None)
+(kps, descs) = sift.detectAndCompute(color, None)
+cv2.drawKeypoints(color, kps, image, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imwrite('sift_keypoints.jpg', image)
 print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
 print(descs)
