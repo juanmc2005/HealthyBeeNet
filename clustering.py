b19304111facd206730f5702180768ab1b54c9f7
Juanma Coria <juanmc2005@hotmail.com>
2018-11-22 16:29:40 +0100

    Added clustering and nearest neighbors algorithms to create a dictionary of visual words and look for them in new images

--- clustering.py
+++ clustering.py
@@ -1,8 +1,28 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
 
-descriptors = np.load('bee_dataset/sift_descriptors.npy')
-kmeans = KMeans(n_clusters=4, random_state=1)
-kmeans.fit(descriptors)
 
-print(kmeans.cluster_centers_)
+def create_kmeans_dict(descriptors_file, k):
+    """
+    Creates a dictionary of descriptors using K Means clustering
+    :param descriptors_file: the file where the descriptor set is stored
+    :param k: the number of clusters to generate
+    :return: a descriptor dictionary of shape (k, d) where d is the descriptor size
+    """
+    descriptors = np.load(descriptors_file)
+    kmeans = KMeans(n_clusters=k, random_state=1)
+    kmeans.fit(descriptors)
+    return kmeans.cluster_centers_
+
+
+def nearest_neghbors(descriptor_dict, descs):
+    """
+    Computes the nearest neighbors of a list of descriptors in a descriptor dictionary
+    :param descriptor_dict: a descriptor dictionary like in create_kmeans_dict
+    :param descs: a list of descriptors of an image
+    :return: the indices of the nearest neighbors of descs in descriptor_dict
+    """
+    nn = NearestNeighbors(n_neighbors=1)
+    nn.fit(descriptor_dict)
+    return nn.kneighbors(descs, return_distance=False).reshape((len(descs),))
--- playground.py
+++ playground.py
@@ -2,6 +2,7 @@ import numpy as np
 from os.path import join
 import image
 import bee_data as bees
+import clustering
 
 
 def compute_descriptors(images_dir, images, out_file=None):
@@ -30,3 +31,9 @@ bee_image_dir = 'bee_dataset/bee_imgs'
 metadata = 'bee_dataset/bee_data.csv'
 train_x, test_x, train_y, test_y = bees.split(bees.read_dataset(metadata))
 
+test_desc = compute_descriptors(bee_image_dir, [test_x[0]])
+
+desc_dict = clustering.create_kmeans_dict('bee_dataset/sift_descriptors_train.npy', 10)
+test_hist = clustering.nearest_neghbors(desc_dict, test_desc)
+
+print(test_hist)
