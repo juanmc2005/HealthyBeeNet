311b5c37bebe7b99d7cc0fcb3b1fab1f96f6553d
Juanma Coria <juanmc2005@hotmail.com>
2018-11-26 18:59:06 +0100

    Added function to train a naive bayes model

--- classification.py
+++ classification.py
@@ -1,6 +1,7 @@
 import image as img
 import clustering as clt
 from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import precision_score, recall_score
 
 
@@ -29,6 +30,21 @@ def train_svm(img_dir, train_x, train_y, desc_dict):
     svc = SVC()
     svc.fit(histograms, train_y)
     return svc
+
+
+def train_naive_bayes(img_dir, train_x, train_y, desc_dict):
+    """
+    Train a Naive Bayes model to classify beehive health based on an image descriptor histogram
+    :param img_dir: the directory to look for the image files
+    :param train_x: a list of image files
+    :param train_y: a list of booleans indicating if the hive is healthy for a bee image
+    :param desc_dict: a list of descriptors representing a dictionary of visual words
+    :return: a trained Naive Bayes model
+    """
+    histograms = histograms_by_image(img_dir, train_x, desc_dict)
+    nb = BernoulliNB()
+    nb.fit(histograms, train_y)
+    return nb
 
 
 def evaluate(model, img_dir, test_x, test_y, desc_dict):
