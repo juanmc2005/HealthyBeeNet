c89bde673fa3487faa16c5ab6c968ac7a94b7664
Juanma Coria <juanmc2005@hotmail.com>
2018-12-03 14:00:27 +0100

    Documented find_best_k function

--- optimisation.py
+++ optimisation.py
@@ -5,7 +5,18 @@ from operator import itemgetter
 
 
 def find_best_k(model, img_dir, train_x, train_y, descriptors):
-    ks = utils.sampled_range(30, 40, 4)
+    """
+    Find the best K value (for K Means) for a specific model using
+    10-fold cross validation with K values in range [10, 600].
+    Print intermediate results so there's feedback for the caller
+    :param model: an sklearn model to evaluate
+    :param img_dir: a directory where all the bee images are located
+    :param train_x: a list of image file names corresponding to the training set
+    :param train_y: a list of image labels, where True = healthy and False = unhealthy
+    :param descriptors: a list of SIFT descriptors extracted from the training set
+    :return: the best k for the model
+    """
+    ks = utils.sampled_range(10, 600, 20)
     print("Will try K = " + str(ks))
     results = []
     for k in ks:
