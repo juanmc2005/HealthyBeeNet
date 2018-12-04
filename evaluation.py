18d1569f5d0442dbde8375fa3e39468a7a59a8c3
Juanma Coria <juanmc2005@hotmail.com>
2018-12-04 13:19:11 +0100

    Rewritten code to evaluate model. Added evaluation code in main.py for the SVM

--- evaluation.py
+++ evaluation.py
@@ -14,9 +14,12 @@ def evaluate(model, img_dir, test_x, test_y, desc_dict):
     :param desc_dict: a list of descriptors representing a dictionary of visual words
     :return: (precision, recall)
     """
-    pred_y = model.predict(clf.histograms_by_image(img_dir, test_x, desc_dict))
-    return precision_score(test_y, pred_y, average='micro'), \
-        recall_score(test_y, pred_y, average='micro')
+    histograms = clf.histograms_by_image(img_dir, test_x, desc_dict)
+    pred_y = model.predict(histograms)
+    return {
+        'precision': accuracy_score(test_y, pred_y),
+        'recall': recall_score(test_y, pred_y)
+    }
 
 
 def cross_val(model, img_dir, train_x, train_y, desc_dict):
@@ -44,3 +47,16 @@ def accuracy(model, img_dir, test_x, test_y, desc_dict):
     :return: the accuracy of the model
     """
     return accuracy_score(test_y, model.predict(clf.histograms_by_image(img_dir, test_x, desc_dict)))
+
+
+def recall(model, img_dir, test_x, test_y, desc_dict):
+    """
+    Return the recall score for the current model in the given test dataset
+    :param model: a previously fit model for beehive health classification
+    :param img_dir: a directory where image files are located
+    :param test_x: a list of bee image files for testing
+    :param test_y: a list of booleans indicating if the hive is healthy for a bee image
+    :param desc_dict: a list of descriptors representing a dictionary of visual words
+    :return: the recall of the model
+    """
+    return precision_score(test_y, model.predict(clf.histograms_by_image(img_dir, test_x, desc_dict)))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import bee_data as bees
 import image as imgs
+import classification as clf
 import clustering as clst
 import evaluation as evl
 import optimisation as opt
@@ -14,15 +15,22 @@ train_descriptors_file = dataset_dir + 'sift_descriptors_train.npy'
 train_x = bees.read_x_split(dataset_dir + 'train_x')
 train_y = bees.read_y_split(dataset_dir + 'train_y')
 
+# Read test data
+test_x = bees.read_x_split(dataset_dir + 'test_x')
+test_y = bees.read_y_split(dataset_dir + 'test_y')
+
 # Load training descriptors
 descriptors = imgs.load_descriptors(train_descriptors_file)
 
 # Find best K for Bernoulli Naive Bayes
-opt.find_best_k(BernoulliNB(), bee_image_dir, train_x, train_y, descriptors)
+# opt.find_best_k(BernoulliNB(), bee_image_dir, train_x, train_y, descriptors)
 
 # Find best K for SVM
-k = opt.find_best_k(SVC(C=10, gamma='scale', random_state=1), bee_image_dir, train_x, train_y, descriptors)
+# k = opt.find_best_k(SVC(C=10, gamma='scale', random_state=1), bee_image_dir, train_x, train_y, descriptors)
+k = 36
 
 # Use best SVM K to print its 10-fold cross validation score
 desc_dict = clst.create_kmeans_dict(descriptors, k=k)
-print(evl.cross_val(SVC(C=10, gamma='scale', random_state=1), bee_image_dir, train_x, train_y, desc_dict))
+model = clf.train_svm(bee_image_dir, train_x, train_y, desc_dict)
+print(evl.evaluate(model, bee_image_dir, test_x, test_y, desc_dict))
+# print(evl.cross_val(SVC(C=10, gamma='scale', random_state=1), bee_image_dir, train_x, train_y, desc_dict))
